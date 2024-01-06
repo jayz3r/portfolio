6609d9f0a077ccfc18df605a70da094907f914be
Adil <adil.abdyldaev07@gmail.com>
2024-01-06 17:23:15 +0600

    sec commit

--- src/pages/dataSlider.js
+++ src/pages/dataSlider.js
@@ -1,27 +1,34 @@
+import beacon from '../pages/img/beacon.jpg'
+import oldman from '../pages/img/oldman.jpg'
+import oldman2 from '../pages/img/oldman2.jpg'
+import statue from '../pages/img/statue.jpg'
+import vase from '../pages/img/vase.jpg'
+import vine from '../pages/img/vine.jpg'
+
 const dataSlider = [
     {
         id:1,
-        image: 'public/img/beacon.jpg'
+        image: beacon
     },
     {
         id:2,
-        image: 'public/img/oldman.jpg'
+        image: oldman
     },
     {
         id:3,
-        image: 'public/img/oldman2.jpg'
+        image: oldman2
     },
     {
         id:4,
-        image: 'public/img/statue.jpg'
+        image: statue
     },
     {
         id:5,
-        image: 'public/img/vase.jpg'
+        image: vase
     },
     {
         id:6,
-        image: 'public/img/vine.jpg'
+        image: vine
     },
 ]
 
